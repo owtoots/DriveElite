01430d5363b55945d666080a20528bea39390293
owtoots <owtoots@gmail.com>
2026-04-09 00:39:05 +0800

    Update database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -44,8 +44,8 @@ def get_connection():
     conn.execute('''CREATE TABLE IF NOT EXISTS drivers (
                     id INTEGER PRIMARY KEY AUTOINCREMENT, owner_username TEXT, first_name TEXT, 
                     cursor.execute('''INSERT INTO users 
-                              (username, password, role, full_name, email, age, nationality, address, contact_number, govt_id_img, license_img, signature_img) 
-                              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', payload)
+                    (username, password, role, full_name, email, age, nationality, address, contact_number, govt_id_img, license_img, signature_img) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', payload)
                     is_owner INTEGER DEFAULT 0, govt_id_img TEXT, license_img TEXT, admin_status TEXT DEFAULT 'PENDING')''')
 
     # --- 5. CATEGORIES TABLE ---
