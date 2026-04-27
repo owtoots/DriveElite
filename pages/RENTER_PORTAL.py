a4c2509b3364b96191cd550a9e43f06f1bff3768
owtoots <owtoots@gmail.com>
2026-04-28 07:00:33 +0800

    Update RENTER_PORTAL.py

--- pages/RENTER_PORTAL.py
+++ pages/RENTER_PORTAL.py
@@ -306,7 +306,7 @@ with tabs[0]:
                                             conn.commit()
                                             
                                             # PayMongo API Call
-                                            SECRET_KEY = st.secrets["paymongo_active_key"]
+                                            SECRET_KEY = st.secrets"paymongo_active_key"
                                             pay_amount = int(grand_total * 100) # Centavos
                                             
                                             auth_string = f"{SECRET_KEY}:"
