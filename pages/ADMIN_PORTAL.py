1e88c345ba8bc9427b056164c57fd69576b711ce
owtoots <owtoots@gmail.com>
2026-06-29 12:57:17 +0800

    Update ADMIN_PORTAL.py

--- pages/ADMIN_PORTAL.py
+++ pages/ADMIN_PORTAL.py
@@ -137,7 +137,7 @@ def get_secret(key, default_val=None):
         return default_val
 
 ADMIN_USERNAME = get_secret("admin_username", "masterom")
-ADMIN_PASSWORD = get_secret("qZ822118qq")
+ADMIN_PASSWORD = get_secret("admin_password")
 
 TAX_RATE = 0.02  
 DEFAULT_RENTER_MARKUP = 0.07
